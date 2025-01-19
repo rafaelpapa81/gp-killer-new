2f24fa3c9b69f318fc6252da50f9a85feac2f605
Rafael Papa <rafaelpapa81@gmail.com>
2025-01-18 19:49:05 -0500

    gp-killer-new add topnavbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
+import Topnavbar from "@/components/ui/topnavbar";
 
 export default function Home() {
   return (
     <div>
+      <Topnavbar />
       <h1>Home</h1>
       <Button>Click me</Button>
     </div>
